e4e435ff55cd70c234aaf061dfa44b711e1e06fb
Liliany Nunes <lilianynunes10@gmail.com>
2024-04-02 13:34:10 -0300

    Adiciona atividade fila

---
+++ atividade_fila/Fila.java
@@ -0,0 +1,58 @@
+public class Fila {
+    int tamanho = 0;
+    int[] elementos;
+    int inicio;
+    int fim;
+    int quantidade;
+
+    public void criarFila(int tamanhoMaximo) {
+        this.tamanho = tamanhoMaximo;
+        this.elementos = new int[tamanhoMaximo];
+        this.inicio = 0;
+        this.fim = -1;
+        this.quantidade = 0;
+    }
+    public void destruirFila() {
+        elementos = null;
+    }    
+
+    public boolean filaCheia() {
+        return quantidade == tamanho;
+    }
+
+    public boolean filaVazia() {
+        return quantidade == 0;
+    }
+
+    public void inserirElemento(int elemento) {
+        if (filaCheia()) {
+            System.out.println("Erro: A fila está cheia.");
+            return;
+        }
+        fim = (fim + 1) % tamanho;
+        elementos[fim] = elemento;
+        quantidade++;
+    }
+
+    public Integer removerElemento() {
+        if (filaVazia()) {
+            System.out.println("Erro: A fila está vazia.");
+            return null;
+        }
+        int elementoRemovido = elementos[inicio];
+        inicio = (inicio + 1) % tamanho;
+        quantidade--;
+        return elementoRemovido;
+    }
+
+    public int localizarElemento(int elemento) {
+        int posicao = -1;
+        for (int i = 0; i < tamanho; i++) {
+            if (elementos[i] == elemento) {
+                posicao = i;
+                break;
+            }
+        }
+        return posicao;
+    }
+}
---
+++ atividade_fila/MainFila.java
@@ -0,0 +1,17 @@
+public class MainFila {
+    public static void main(String[] args) {
+        Fila fila = new Fila();
+        fila.criarFila(10);
+        fila.inserirElemento(10);
+        fila.inserirElemento(20);
+        fila.inserirElemento(30);
+        System.out.println("Elemento removido: " + fila.removerElemento());
+        int posicao = fila.localizarElemento(30); 
+        if (posicao != -1) {
+            System.out.println("Elemento 30 encontrado na posição: " + posicao);
+        } else {
+            System.out.println("Elemento 30 não encontrado na fila.");
+        }
+        fila.destruirFila();
+    }
+}
